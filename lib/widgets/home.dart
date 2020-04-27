6a3da98c4162279e70431a2dafd765067e52c9a7
marking <makai554892700@sina.com>
2020-04-27 17:14:43 +0800

    add:添加首页广告

---
+++ lib/widgets/home.dart
@@ -0,0 +1,81 @@
+import 'package:MYSShop/utils/screen_utils.dart';
+import 'package:MYSShop/widgets/view/waiter_view.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  State createState() {
+    return HomeState();
+  }
+}
+
+class HomeState extends State<Home> {
+  bool showAd = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Offstage(
+          child: Text('Hello world.'),
+          offstage: showAd,
+        ),
+        Offstage(
+          child: Container(
+            child: Stack(
+              children: [
+                Image(
+                  image: AssetImage('images/lion.jpg'),
+                  width: double.infinity,
+                  height: double.infinity,
+                  fit: BoxFit.cover,
+                ),
+                Positioned(
+                  bottom: 80.0,
+                  child: SizedBox(
+                    width: ScreenUtils.screenW(context),
+                    child: Text(
+                      '落花有意随流水,流水无心恋落花',
+                      style: TextStyle(fontSize: 15.0, color: Colors.white),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                SafeArea(
+                  child: Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      child: Waiter(
+                        seconds: 15,
+                        text: '跳过',
+                        onCountDownFinishCallBack: (bool value) {
+                          if (value) {
+                            setState(() {
+                              showAd = false;
+                            });
+                          }
+                        },
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                        color: Colors.white,
+                      ),
+                      padding: EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 5.0),
+                      margin: EdgeInsets.only(right: 20.0, top: 20.0),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            color: Colors.white,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+          offstage: !showAd,
+        ),
+      ],
+    );
+  }
+}
